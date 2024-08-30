11934fa926c802bb1ddc9c04d4b6a6a9f907558e
Bruno <brunoscosta17@gmail.com>
2024-08-29 23:43:40 -0300

    feat(product-search): add search to header

--- modules/feature/product/search/src/lib/product-search/product-search.component.ts
+++ modules/feature/product/search/src/lib/product-search/product-search.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { productMock } from '@ecommerce-mentoria-angular/product-data-access';
 @Component({
   selector: 'lib-product-search',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    MatFormFieldModule,
+  ],
   templateUrl: './product-search.component.html',
   styleUrl: './product-search.component.scss',
 })
-export class ProductSearchComponent {}
+export class ProductSearchComponent {
+  control = new FormControl('');
+  options: string[] = ['One', 'Two', 'Three'];
+  products = productMock;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,17 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { LayoutModule } from '@ecommerce-mentoria-angular/layout';
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { ProductSearchComponent } from '@ecommerce-mentoria-angular/product-search';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, LayoutModule],
+  imports: [
+    NxWelcomeComponent,
+    RouterModule,
+    LayoutModule,
+    ProductSearchComponent,
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
