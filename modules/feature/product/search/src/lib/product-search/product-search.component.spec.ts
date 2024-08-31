dbe84ce2a31ab0840770621db80ab62df2083031
Bruno <brunoscosta17@gmail.com>
2024-08-31 18:42:49 -0300

    First commit

--- modules/feature/product/search/src/lib/product-search/product-search.component.spec.ts
+++ modules/feature/product/search/src/lib/product-search/product-search.component.spec.ts
@@ -2,6 +2,11 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProductSearchComponent } from './product-search.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
+import {
+  productMock,
+  ProductSearchService,
+} from '@ecommerce-mentoria-angular/product-data-access';
+import { of } from 'rxjs';
 
 describe('ProductSearchComponent', () => {
   let component: ProductSearchComponent;
@@ -10,7 +15,19 @@ describe('ProductSearchComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ProductSearchComponent, NoopAnimationsModule],
-      providers: [provideHttpClientTesting()],
+      providers: [
+        provideHttpClientTesting(),
+        {
+          provide: ProductSearchService,
+          useValue: {
+            searchByName: () => {
+              return {
+                subscribe: () => of(productMock),
+              };
+            },
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProductSearchComponent);
