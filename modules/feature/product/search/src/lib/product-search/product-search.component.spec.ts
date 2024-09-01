6f72d7bdd756d08764e9e9e8ca70f11b785c594c
Bruno <brunoscosta17@gmail.com>
2024-09-01 15:07:40 -0300

    tests(product-search): adding tests to component

--- modules/feature/product/search/src/lib/product-search/product-search.component.spec.ts
+++ modules/feature/product/search/src/lib/product-search/product-search.component.spec.ts
@@ -1,4 +1,9 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 import { ProductSearchComponent } from './product-search.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
@@ -11,6 +16,8 @@ import { of } from 'rxjs';
 describe('ProductSearchComponent', () => {
   let component: ProductSearchComponent;
   let fixture: ComponentFixture<ProductSearchComponent>;
+  let productSearchService: ProductSearchService;
+  let input: HTMLInputElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -22,20 +29,92 @@ describe('ProductSearchComponent', () => {
           useValue: {
             searchByName: () => {
               return {
-                subscribe: () => of(productMock),
+                searchByName: jest.fn().mockReturnValue(of(productMock)),
               };
             },
           },
         },
       ],
     }).compileComponents();
-
+    productSearchService = TestBed.inject(ProductSearchService);
     fixture = TestBed.createComponent(ProductSearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    input = fixture.nativeElement.querySelector('input');
   });
+
+  function simulateInput(value: string) {
+    input.value = value;
+    input.dispatchEvent(new Event('input'));
+  }
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should debounce when input field is changed', fakeAsync(() => {
+    jest
+      .spyOn(productSearchService, 'searchByName')
+      .mockReturnValue(of(productMock));
+
+    simulateInput('tv');
+
+    expect(productSearchService.searchByName).not.toHaveBeenCalled();
+
+    tick(500); // debounce time
+
+    expect(productSearchService.searchByName).toHaveBeenCalledWith(input.value);
+  }));
+
+  it('should search multiple times', fakeAsync(() => {
+    jest
+      .spyOn(productSearchService, 'searchByName')
+      .mockReturnValue(of(productMock)); // Verifique se o mock está correto
+
+    simulateInput('tv');
+
+    tick(500); // Tempo para debounce
+
+    simulateInput('phone');
+
+    tick(500); // Tempo para debounce
+
+    expect(productSearchService.searchByName).toHaveBeenCalledTimes(2);
+  }));
+
+  it('should prevent identical submissions', fakeAsync(() => {
+    jest
+      .spyOn(productSearchService, 'searchByName')
+      .mockReturnValue(of(productMock));
+
+    simulateInput('tv');
+
+    tick(500); // Tempo para debounce
+
+    simulateInput('tv');
+
+    tick(500); // Tempo para debounce
+
+    expect(productSearchService.searchByName).toHaveBeenCalledTimes(1);
+  }));
+
+  it('should prevent empty submissions', fakeAsync(() => {
+    jest
+      .spyOn(productSearchService, 'searchByName')
+      .mockReturnValue(of(productMock));
+
+    simulateInput('');
+
+    tick(500); // Tempo para debounce
+
+    expect(productSearchService.searchByName).not.toHaveBeenCalled();
+  }));
+
+  it('should return products observable correctly', () => {
+    const products = component.products$;
+    expect(products).toBeTruthy();
+    component.products$.subscribe((result) => {
+      expect(result).toEqual(productMock);
+    });
   });
 });
