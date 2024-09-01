6a66cbf2d6c8a5b76eeb02164c63d4c26e88a165
Bruno <brunoscosta17@gmail.com>
2024-09-01 19:02:50 -0300

    feat(home): setting lazy load

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('@ecommerce-mentoria-angular/home').then((c) => c.homeRoutes),
+    // loadComponent: () => import('@ecommerce-mentoria-angular/home').then(c => c.HomeComponent)
+  },
+];
