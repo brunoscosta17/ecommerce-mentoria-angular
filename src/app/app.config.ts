96f7abb1a7578ecee6b4f8359aa3099710c7b615
Bruno <brunoscosta17@gmail.com>
2024-09-01 19:42:03 -0300

    feat(home): setting lazy load

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import {
-  PreloadAllModules,
-  provideRouter,
-  withPreloading,
-} from '@angular/router';
+import { NoPreloading, provideRouter, withPreloading } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -11,7 +7,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes, withPreloading(PreloadAllModules)),
+    provideRouter(appRoutes, withPreloading(NoPreloading)),
     provideHttpClient(),
     provideAnimationsAsync(),
   ],
