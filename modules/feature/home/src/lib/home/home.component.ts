04af52b194022a78d37dc3ef4b84a13a61c8b772
Bruno <brunoscosta17@gmail.com>
2024-09-15 12:18:35 -0300

    feat(home): add href recommended products

--- modules/feature/home/src/lib/home/home.component.ts
+++ modules/feature/home/src/lib/home/home.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { Product } from 'modules/data-access/product/src/lib/models/product.model';
+import { productMock } from '@ecommerce-mentoria-angular/product-data-access';
 
 @Component({
   selector: 'lib-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  products: Product[] = productMock;
+}
