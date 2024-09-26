54ce84ff5c7dbf54aa15bce63c50a2dcbbe303fc
Bruno <brunoscosta17@gmail.com>
2024-09-25 22:56:47 -0300

    feat(product-data-access): add recommended products

---
+++ modules/data-access/product/src/lib/services/recommended-products/recommended-products.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Product } from '../../models/product.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecommendedProductsService {
+  readonly apiUrl = 'https://65009f9718c34dee0cd53786.mockapi.io/';
+
+  constructor(private _http: HttpClient) {}
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get<Product[]>(`${this.apiUrl}/products`, {
+      params: { page: 1, limit: 5 },
+    });
+  }
+}
